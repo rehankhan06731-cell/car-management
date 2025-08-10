5080c0ac23fb323a0932062aa899a3b4559a7bb1
rehankhan06731-cell <rehankhan06731@gmail.com>
2025-08-10 18:20:56 +0500

    App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './components/Layout';
+import CarList from './components/CarList';
+import './App.css';
+
+export default function App() {
+  return (
+    <Layout>
+      <CarList />
+    </Layout>
+  );
+}
